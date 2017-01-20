06b87521a27872a1bbbc7fa2b7fba2790a692226
georgistoilov8 <gogo.stoilov@abv.bg>
2017-01-20 15:37:45 +0200

    Fixing issues in lamps script

--- The Maze/Assets/Scripts/Light/LightScript.cs
+++ The Maze/Assets/Scripts/Light/LightScript.cs
@@ -7,6 +7,7 @@ public class LightScript : MonoBehaviour {
 	private List<GameObject> lamps = new List<GameObject>();
 	private GameObject lampPuzzle;
 	private GameObject player;
+	private Plane[] planes;
 
 	public float forwardSpace;
 	public float backwardSpace;
@@ -16,6 +17,7 @@ public class LightScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		planes = GeometryUtility.CalculateFrustumPlanes (Camera.main);
 		player = GameObject.FindGameObjectWithTag ("Player");
 		lampPuzzle = GameObject.FindGameObjectWithTag ("Lamps");
 		foreach (Transform child in lampPuzzle.transform)
@@ -27,15 +29,28 @@ public class LightScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		Vector3 CameraDirectionFrontX = new Vector3 (Camera.main.transform.forward.x + 9f, Camera.main.transform.forward.y, Camera.main.transform.forward.z + 9f);
+		//Vector3 CameraDirectionFrontZ = Camera.main.transform.forward.z + 15f;
+		//Vector3 CameraDirectionBack = -CameraDirectionFront;
+		//Vector3 CameraDirectionRight = Camera.main.transform.right;
+		//Vector3 CameraDirectionLeft = -CameraDirectionRight;
+		//Debug.Log (CameraDirectionBack + "   " + CameraDirectionFront);
 		foreach (GameObject child in lamps) 
 		{
-			if (child.transform.position.x <= (player.transform.position.x + forwardSpace) && child.transform.position.x >= (player.transform.position.x - backwardSpace) &&
-			   child.transform.position.z <= (player.transform.position.z + leftSpace) && child.transform.position.z >= (player.transform.position.z - rightSpace)) {
+			Collider childCollider = child.GetComponent<Collider> ();
+			if (GeometryUtility.TestPlanesAABB (planes, childCollider.bounds)) {
 				child.SetActive (true);
 			} else 
 			{
 				child.SetActive (false);
 			}
+			//if (Vector3.SqrMagnitude(child.transform.position - CameraDirectionFront) > -15 && child.transform.position.x >= (CameraDirectionBack.x - backwardSpace) &&
+				//child.transform.position.z <= (CameraDirectionLeft.z + leftSpace) && child.transform.position.z >= (CameraDirectionRight.z - rightSpace)) {
+				//child.SetActive (true);
+			//} else 
+			//{
+				//child.SetActive (false);
+			//
 		}
 	}
 }
