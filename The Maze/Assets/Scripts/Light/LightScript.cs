0f8b0c40fa60e5a0d73831d7a17c64e3931272ff
georgistoilov8 <gogo.stoilov@abv.bg>
2017-01-25 11:41:39 +0200

    Made better lights. Maze 1 almost done. Add more stuff to Maze 1

--- The Maze/Assets/Scripts/Light/LightScript.cs
+++ The Maze/Assets/Scripts/Light/LightScript.cs
@@ -22,14 +22,14 @@ public class LightScript : MonoBehaviour {
 		lampPuzzle = GameObject.FindGameObjectWithTag ("Lamps");
 		foreach (Transform child in lampPuzzle.transform)
 		{
-			Debug.Log (child);
+			Debug.Log (child.transform.position);
 			lamps.Add (child.gameObject);
 		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		Vector3 CameraDirectionFrontX = new Vector3 (Camera.main.transform.forward.x + 9f, Camera.main.transform.forward.y, Camera.main.transform.forward.z + 9f);
+		//Vector3 CameraDirectionFrontX = new Vector3 (Camera.main.transform.forward.x + 9f, Camera.main.transform.forward.y, Camera.main.transform.forward.z + 9f);
 		//Vector3 CameraDirectionFrontZ = Camera.main.transform.forward.z + 15f;
 		//Vector3 CameraDirectionBack = -CameraDirectionFront;
 		//Vector3 CameraDirectionRight = Camera.main.transform.right;
@@ -37,20 +37,24 @@ public class LightScript : MonoBehaviour {
 		//Debug.Log (CameraDirectionBack + "   " + CameraDirectionFront);
 		foreach (GameObject child in lamps) 
 		{
-			Collider childCollider = child.GetComponent<Collider> ();
-			if (GeometryUtility.TestPlanesAABB (planes, childCollider.bounds)) {
-				child.SetActive (true);
-			} else 
-			{
-				child.SetActive (false);
-			}
-			//if (Vector3.SqrMagnitude(child.transform.position - CameraDirectionFront) > -15 && child.transform.position.x >= (CameraDirectionBack.x - backwardSpace) &&
-				//child.transform.position.z <= (CameraDirectionLeft.z + leftSpace) && child.transform.position.z >= (CameraDirectionRight.z - rightSpace)) {
-				//child.SetActive (true);
+			//Collider childCollider = child.GetComponent<Collider> ();
+			//if (GeometryUtility.TestPlanesAABB (planes, childCollider.bounds)) {
+			//	child.SetActive (true);
 			//} else 
 			//{
 				//child.SetActive (false);
-			//
+			//}
+
+			if (child.transform.position.x < (player.transform.position.x + forwardSpace) && 
+				child.transform.position.x > (player.transform.position.x - backwardSpace) &&
+					child.transform.position.z > (player.transform.position.z - leftSpace) && 
+						child.transform.position.z < (player.transform.position.z + rightSpace)) 
+			{
+				child.SetActive (true);
+			} else {
+				if(child.activeSelf.Equals(true))
+					child.SetActive (false);
+			}
 		}
 	}
 }
